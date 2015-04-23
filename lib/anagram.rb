b0bb42a2b12392c815f9bf506ae6185e86f54484
Alex Caste & David Abel <student@epicodus.com>
2015-04-23 14:58:06 -0700

    Successfully splits dog into an array

---
+++ lib/anagram.rb
@@ -0,0 +1,7 @@
+class String
+
+  define_method(:grams) do
+     ana_array = []
+     ana_array = self.split("")
+  end
+end
--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -4,6 +4,6 @@ require('anagram')
 
 describe('String#grams') do
     it('splits word into an array') do
-        expect("dog".grams()).to(eql?(["d", "o", "g"]))
+        expect("dog".grams()).to(eql(["d", "o", "g"]))
     end
 end
