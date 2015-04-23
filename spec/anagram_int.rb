0b9a5a468c9a71a4c93e48b6f1447dc4f595b6ee
Alex Caste & David Abel <student@epicodus.com>
2015-04-23 14:49:15 -0700

    First spect split word into array written and fails

--- spec/anagram_int.rb
+++ spec/anagram_int.rb
@@ -1,2 +1,4 @@
 require('capybara/rspec')
 require('./app')
+
+set(:show_exceptions, false)
--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -1,3 +1,9 @@
 require('rspec')
 require('pry')
 require('anagram')
+
+describe('String#grams') do
+    it('splits word into an array') do
+        expect("dog".grams()).to(eql?(["d", "o", "g"]))
+    end
+end
