65195a9e6d5036d483f32883ad2d094b98da92f9
Alex Caste & David Abel <student@epicodus.com>
2015-04-23 15:00:00 -0700

    Add second spec sort array

--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -6,4 +6,8 @@ describe('String#grams') do
     it('splits word into an array') do
         expect("dog".grams()).to(eql(["d", "o", "g"]))
     end
+    it('a sort array') do
+        expect("dog".grams()).to(eql(["d", "g", "o"]))
+    end
+
 end
